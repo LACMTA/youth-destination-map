24270eefa5bec8b419dae287abb46eaf84318cc9
Nina Kin <matikin9@gmail.com>
2024-07-26 21:35:00 -0700

    update for new lambda functions

--- README.md
+++ README.md
@@ -2,19 +2,28 @@
 
 ## Destinations
 
-We asked attendees of the 2023 Youth Summit, the members of the 2024 Youth Council, and peers of the 2024 Youth council to provide destinations for this map - places they like to visit, hidden gems that other teens should know about, etc.
+Attendees of the 2023 Youth Summit, the members of the 2024 Youth Council, and peers of the 2024 Youth council were asked to contribute destinations for this guide: places they like to visit, hidden gems that other teens should know about, etc.
 
-We use the Google Places API to auto-suggest locations and we only save destinations that are in Google's database.
+The Google Places API is used to auto-suggest locations for contributors to choose from.
 
-The resulting data still needed to be filtered out for:
+In 2024, we started asking contributors to provide their first name and the reason for their recommendation.
 
-* Airports, airlines
-* K-12 schools
-* Places outside of LA County that Metro does not serve.
-* Places that were not specific enough
-* Places where it was unclear why it was being recommended.
-* Ferries, islands, cruise piers
+## Tech Stack
 
-Additionally, duplicates needed to be combined.
+This website is built using 11ty and hosted on GitHub.
 
-In 2024, users were also asked to submit their first name and why they were recommending a place.
+### Form Submissions
+
+Destinations submitted through the form are passed to an AWS Lambda function, which will then store the data in an Airtable base.
+
+The Airtable base has automations that will take the form inputs and separate the data into two separate tables: one for destinations, and one for recommendations.  Each recommendation is tied to a destination.
+
+### Map
+
+The map calls an AWS Lambda function that reads from the recommendations table in Airtable, and it returns JSON data that is an array of destination objects, each with an array of recommendations.
+
+MapLibre is used to create the map, markers, and popups.  The basemap uses a hybrid vector/rastor basemap created by Metro to include rail lines, busways, and stations.
+
+## Contributing
+
+Suggestions on how improve the code and infrastructure on this project are always welcome!
---
+++ docs/js/scripts-map-final.js
@@ -0,0 +1,165 @@
+const ESRI_KEY =  "AAPKccc2cf38fecc47649e91529acf524abflSSkRTjWwH0AYmZi8jaRo-wdpcTf6z67CLCkOjVYlw3pZyUIF_Y4KGBndq35Y02z";
+const basemapEnum = "65aff2873118478482ec3dec199e9058";
+
+const destinationsDataFile = '/youth-destination-map/data/destinations-data.csv';
+const recommendationsDataFile = '/youth-destination-map/data/recommendations-data.csv';
+
+const readFromAirtableUrl = 'https://ycvplis7qloqh5g6qjmwgvmusq0twgct.lambda-url.us-west-1.on.aws/';
+const getDestUrl = 'https://5rqyajkoanm2z7nfkf7fcdxxt40fdxzi.lambda-url.us-west-1.on.aws/';
+const getRecsUrl = 'https://6h4liuthvnsgu7morkntsf55ai0jnfra.lambda-url.us-west-1.on.aws/';
+
+
+
+async function getData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+
+        const jsonData = await response.json();
+        console.log(jsonData);
+        
+        return jsonData;
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+var map = new maplibregl.Map({
+    container: 'map', // container id
+    style: 'https://demotiles.maplibre.org/style.json', // style URL
+    center: [-118.25133692966446, 34.00095151499077], 
+    zoom: 10,
+    style: `https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/styles/items/${basemapEnum}?token=${ESRI_KEY}`,
+});
+
+map.on('load', () => {
+    new mapboxglEsriSources.TiledMapService('imagery-source', map, {
+        url: 'https://tiles.arcgis.com/tiles/TNoJFjk1LsD45Juj/arcgis/rest/services/Map_RGB_Vector_Offset_RC5/MapServer'
+    });
+
+    map.addLayer({
+        id: 'imagery-layer',
+        type: 'raster',
+        source: 'imagery-source'
+    })
+
+    Promise.all([getData(getDestUrl), getData(getRecsUrl)])
+    .then(results => {
+        let destinations = results[0];
+        let recommendations = results[1];
+
+        createMarkersSeparate(destinations, recommendations);
+
+        console.log('Destinations: ' + destinations.length + ' Recommendations: ' + recommendations.length);
+    })
+    .catch(err => console.error(err));
+
+    // Promise.all([parseCSV(destinationsDataFile), parseCSV(recommendationsDataFile)])
+    // .then(results => {
+    //     // results[0] contains data from destinationsDataFile
+    //     // results[1] contains data from recommendationsDataFile
+    //     console.log('Parsed ' + results[0].length + ' destinations and ' + results[1].length + ' recommendations');
+
+    //     // Filter destinations for non-null coordinates and show = true        
+    //     let filteredDestinations = results[0].filter(function(row) {
+    //         return row.lat != null && row.lon != null && row.show === "TRUE";
+    //     });
+        
+    //     console.log('Filtered out ' + filteredDestinations.length + ' destinations');
+    //     createMarkersLocal(filteredDestinations, results[1]);
+    // })
+    // .catch(err => console.error(err));
+
+});
+
+function createMarkersSeparate(destinations, recommendations) {
+    destinations.forEach(destination => {
+        let popupContent = `<span class="popupDestination"><strong>${destination.google_place}</strong></span>`;
+        let matchingRecommendations = recommendations.filter(recommendation => recommendation.google_place_id === destination.google_place_id);
+
+        popupContent += `<div>`;
+
+        if (matchingRecommendations.length > 0) {
+            matchingRecommendations[0].recommendations.forEach(rec => {
+                popupContent += `<p>"${rec.description}" <br><strong>- <em>${rec.first_name}</em></strong></p>`;
+            });
+        }
+        popupContent += `</div>`;
+        createMarker(destination, popupContent);
+    });
+}
+
+// function createMarkersLambda(destinations) {
+//     destinations.forEach(destination => {
+//         let popupContent = `<strong>${destination.google_place}</strong>`;
+//         let recommendations = destination.recommendations;
+
+//         recommendations.forEach(recommendation => {
+//             popupContent += `<p>"${recommendation.description}" <br>- <em>${recommendation.first_name}</em></p>`;
+//         });
+
+//         createMarker([destination.lon, destination.lat], popupContent);
+//     });
+// }
+
+// function createMarkersLocal(destinations, recommendations) {
+//     destinations.forEach(destination => {
+//         let popupContent = `<strong>${destination.user_entered_place}</strong>`;
+
+//         let matchingRecommendations = recommendations.filter(recommendation => recommendation.google_place_id === destination.google_place_id);
+
+//         if (matchingRecommendations.length > 0) {
+//             matchingRecommendations.forEach(recommendation => {
+//                 popupContent += `<p>"${recommendation.description}" <br>- <em>${recommendation.first_name}</em></p>`;
+//             });
+//         }
+
+//         createMarker([destination.lon, destination.lat], popupContent);
+//     });
+// }
+
+function createMarker(destination, content) {
+    let iconPath = destination['category_icon_file'];
+    const popup = new maplibregl.Popup({offset: 25})
+    .setHTML(content);
+
+    // create a DOM element for the marker
+    const el = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = `../img/${iconPath}`;
+    img.style.width = '24px';
+    img.style.height = '24px';
+
+    el.className = 'markerIcon';
+    el.style.backgroundPosition = 'center';
+    el.style.backgroundRepeat = 'no-repeat';
+    el.style.width = '30px';
+    el.style.height = '30px';
+    if (iconPath != 'marker.svg') {
+        el.appendChild(img);
+    }
+    
+
+    const marker = new maplibregl.Marker({element: el})
+    .setLngLat([destination.lon, destination.lat])
+    .setPopup(popup)
+    .addTo(map);
+}
+
+// Create a function to parse a CSV file
+function parseCSV(file) {
+    return new Promise((resolve, reject) => {
+        Papa.parse(file, {
+            download: true,
+            header: true,
+            complete: function(results) {
+                resolve(results.data);
+            },
+            error: function(err) {
+                reject(err);
+            }
+        });
+    });
+}
