d636808e9c688dea5cbbdeeed5d566de17186a48
Nina Kin <matikin9@gmail.com>
2024-09-03 12:26:53 -0700

    clean up styling

--- docs/js/site-footer.js
+++ docs/js/site-footer.js
@@ -1,7 +1,7 @@
-const mapDiv = document.querySelector('#map-container');
-let map;
+// const mapDiv = document.querySelector('#map-container');
+// let map;
 
-map = mapDiv ? L.map(mapDiv).setView([34.0622, -118.2437], 10) : null;
+// map = mapDiv ? L.map(mapDiv).setView([34.0622, -118.2437], 10) : null;
 
 const docHeight = document.body.scrollHeight;
 
@@ -20,53 +20,53 @@ const options = {
     strictBounds: true
 }
 let autocomplete;
-let markers = L.layerGroup();
+// let markers = L.layerGroup();
 
 document.addEventListener('DOMContentLoaded', () => {
-    if (map) {
-        let url = window.location.href;
-        let refreshInteral = 15000; // 15 seconds
+    // if (map) {
+    //     let url = window.location.href;
+    //     let refreshInteral = 15000; // 15 seconds
     
-        console.log(url);
+    //     console.log(url);
     
-        if (url.includes('/map/')) {
-            console.log('on map page setting zoom to 11');
-            map.setZoom(11);
-        } else {
-            console.log('NOT on map page');
-            refreshInteral = 30000;
-        }
+    //     if (url.includes('/map/')) {
+    //         console.log('on map page setting zoom to 11');
+    //         map.setZoom(11);
+    //     } else {
+    //         console.log('NOT on map page');
+    //         refreshInteral = 30000;
+    //     }
 
-        /* This is a Carto-styled OSM basemap*/
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x.png' : '.png'), {
-            attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
-            subdomains: 'abcd',
-            maxZoom: 19,
-            minZoom: 0
-        }).addTo(map);
+    //     /* This is a Carto-styled OSM basemap*/
+    //     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x.png' : '.png'), {
+    //         attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    //         subdomains: 'abcd',
+    //         maxZoom: 19,
+    //         minZoom: 0
+    //     }).addTo(map);
 
-        const wmsLayer = L.tileLayer('https://tiles.arcgis.com/tiles/TNoJFjk1LsD45Juj/arcgis/rest/services/Map_RGB_Vector_Offset_RC4/MapServer/WMTS/tile/1.0.0/Map_RGB_Vector_Offset_RC4/default/default028mm/{z}/{y}/{x}.png').addTo(map);
+    //     const wmsLayer = L.tileLayer('https://tiles.arcgis.com/tiles/TNoJFjk1LsD45Juj/arcgis/rest/services/Map_RGB_Vector_Offset_RC4/MapServer/WMTS/tile/1.0.0/Map_RGB_Vector_Offset_RC4/default/default028mm/{z}/{y}/{x}.png').addTo(map);
 
-        window.setInterval(readFromAirtable, refreshInteral);
-        // readFromAirtable();
-    }
+    //     window.setInterval(readFromAirtable, refreshInteral);
+    //     // readFromAirtable();
+    // }
     
 
     let form = document.getElementById('form')
     if (form) {
         form.addEventListener('submit', handleSubmit);
 
-        if (map) {
-            document.body.addEventListener('click', function(e) {
-                if (e.target.closest('#destination')) {
-                    document.getElementById('map-container').style.flexGrow = "1";
-                    document.getElementById('form-container').style.flexGrow = "4";
-                } else {
-                    document.getElementById('map-container').style.flexGrow = "4";
-                    document.getElementById('form-container').style.flexGrow = "1";
-                }
-            });
-        }
+        // if (map) {
+        //     document.body.addEventListener('click', function(e) {
+        //         if (e.target.closest('#destination')) {
+        //             document.getElementById('map-container').style.flexGrow = "1";
+        //             document.getElementById('form-container').style.flexGrow = "4";
+        //         } else {
+        //             document.getElementById('map-container').style.flexGrow = "4";
+        //             document.getElementById('form-container').style.flexGrow = "1";
+        //         }
+        //     });
+        // }
     }
 
     
