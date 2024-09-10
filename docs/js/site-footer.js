a1d6d38fe99e24e8e8c4199a0ba5d7d2dbdaca56
Nina Kin <matikin9@gmail.com>
2024-09-10 16:46:18 -0700

    add category field and fix some input gaps

--- docs/js/site-footer.js
+++ docs/js/site-footer.js
@@ -16,32 +16,57 @@ const options = {
 }
 let autocomplete;
 
+// Callback function for Google Map API
+// as defined in src/_includes/default.liquid
+function initMap() {
+    console.log('Gmaps call returned');
+    if (input !== null) {
+        autocomplete = new google.maps.places.Autocomplete(input, options);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     let form = document.getElementById('form')
     if (form) {
         form.addEventListener('submit', handleSubmit);
     }
+
+    document.getElementById('destination').addEventListener('input', handleInputChanged);
 })
+
+function handleInputChanged(event) {
+    autocomplete.set('place',null);
+}
 
 function handleSubmit(event) {
     document.getElementById('confirmation').innerText = '';
     document.getElementById('saving').style.display = 'block';
-    saveToAirtable();
-    document.getElementById('destination').value = '';
-    document.getElementById('first-name').value = '';
-    document.getElementById('description').value = '';
-    autocomplete.set('place', null);
-    event.preventDefault();
+
+    let google_place = autocomplete.getPlace();
+    if (google_place == null || !google_place.geometry) {
+        document.getElementById('destination').value = '';
+        document.getElementById('confirmation').innerHTML = '<br><br>Please select a destination from the dropdown';
+        document.getElementById('saving').style.display = 'none';
+        event.preventDefault();
+    } else {
+        saveToAirtable();
+        document.getElementById('destination').value = '';
+        document.getElementById('first-name').value = '';
+        document.getElementById('description').value = '';
+        document.getElementById('category').value = '';
+        autocomplete.set('place', null);
+        event.preventDefault();
+    }
 }
 
 function saveToAirtable() {
     let google_place = autocomplete.getPlace();
     if (google_place == null) {
         document.getElementById('destination').value = '';
-        document.getElementById('confirmation').innerText = 'Please select from the dropdown';
+        document.getElementById('confirmation').innerHTML = '<br><br>Please select a destination from the dropdown';
     } else if (!google_place.geometry) {
-        document.getElementById('confirmation').innerText = 'Please select from the dropdown';
-        document.getElementById('autocomplete').placeholder = 'Where do you want to go?';
+        document.getElementById('confirmation').innerHTML = '<br><br>Please select a destination from the dropdown';
+        // document.getElementById('autocomplete').placeholder = 'Where do you want to go?';
     } else {
         console.log(google_place.name);
         console.log(google_place.place_id);
@@ -58,13 +83,14 @@ function saveToAirtable() {
         lambda_airtable_url += '&user_entered_place=' + shortenedPlaceName;
         lambda_airtable_url += '&first_name=' + document.getElementById('first-name').value;
         lambda_airtable_url += '&description=' + document.getElementById('description').value;
+        lambda_airtable_url += '&category=' + document.getElementById('category').value;
         
         console.log('Lambda write call: ' + lambda_airtable_url);
 
         postInsertData(lambda_airtable_url).then((data) => {
             console.log('write call return: ' + data);
             document.getElementById('saving').style.display = 'none';
-            document.getElementById('confirmation').innerText = data + ' found via Google and added!';
+            document.getElementById('confirmation').innerHTML = '<br><br>' + data + ' found via Google and added!';
         });
     }
 }
